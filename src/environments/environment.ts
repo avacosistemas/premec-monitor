f16158f4148928c2a4d276d752193f4c4f53b243
Avaco Sistemas <avacosistemas@gmail.com>
2024-07-04 12:06:27 -0300

    ajustes monitor para mostrar amarillos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-//export const PREFIX_DOMAIN_API =  'http://premec.ddns.net:48080/ws-rest/';
+// export const PREFIX_DOMAIN_API =  'http://premec.ddns.net:48080/ws-rest/';
 export const PREFIX_DOMAIN_API =  'http://localhost:8080/ws-rest/';
 
 export const environment = {
